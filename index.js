00045ebbd76c27f9e4ce0f304e26400fa3c6c5c2
vsmaliakou <vsmaliakou@yandex.by>
2021-06-29 19:01:31 +0300

    added env variables

--- index.js
+++ index.js
@@ -7,15 +7,17 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
-const port = 3010
+let port = process.env.PORT || 3010
+let smtp_login = process.env.SMTP_LOGIN || "---"
+let smtp_password = process.env.SMTP_PASSWORD || "---"
 
 let transporter = nodemailer.createTransport({
     host: "smtp.yandex.ru",
     port: 465,
     secure: true,
     auth: {
-        user: "vsmaliakou@yandex.by",
-        pass: "VS9121993yandex",
+        user: smtp_login,
+        pass: smtp_password,
     },
 });
 
@@ -32,18 +34,16 @@ app.post('/sendMessage', async (req, res) => {
         to: "vsmaliakou@yandex.by",
         subject: "HR WANTS ME",
         html: `<b>Сообщение с Personal Portfolio</b>
-<div>
-name: ${name}
-</div>
-<div>
-contacts: ${contacts}
-</div>
-<div>
-${message}
-</div>`,
+            <div>
+                name: ${name}
+            </div>
+            <div>
+                contacts: ${contacts}
+            </div>
+            <div>
+                ${message}
+            </div>`,
     });
-
-
     res.send('ok')
 })
 
